d499d2c5d8a68ab3588e10e86c939caaa45e4059
Cabral Williams <cabral.williams@gmail.com>
2022-03-11 19:05:35 -0600

    Project startup

---
+++ src/main/java/com/datatracker/javadatatracker/JavaDataTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.datatracker.javadatatracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDataTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDataTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datatracker/javadatatracker/JavaDataTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datatracker.javadatatracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDataTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
